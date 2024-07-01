5e26dabd7e1556b560eeae9dc784e5f07c74ad99
milo.miao <wenluma_521@163.com>
2024-07-01 22:06:48 +0800

    p2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import TodoForm from './components/TodoForm';
@@ -6,11 +6,16 @@ import TodoList from './components/TodoList';
 import './App.css';
 
 function App() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <Provider store={store}>
       <div className="App">
         <h1>Todo List</h1>
-        <TodoForm />
+        <button className="add-todo-btn" onClick={() => setShowForm(!showForm)}>
+          {showForm ? 'Close' : 'Add New Todo'}
+        </button>
+        {showForm && <TodoForm />}
         <TodoList />
       </div>
     </Provider>
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -39,16 +39,18 @@ const TodoForm = () => {
         value={detail}
         onChange={(e) => setDetail(e.target.value)}
       />
-      <input
-        type="datetime-local"
-        value={startTime}
-        onChange={(e) => setStartTime(e.target.value)}
-      />
-      <input
-        type="datetime-local"
-        value={endTime}
-        onChange={(e) => setEndTime(e.target.value)}
-      />
+      <div className="time-inputs">
+        <input
+          type="datetime-local"
+          value={startTime}
+          onChange={(e) => setStartTime(e.target.value)}
+        />
+        <input
+          type="datetime-local"
+          value={endTime}
+          onChange={(e) => setEndTime(e.target.value)}
+        />
+      </div>
       <button type="submit">Add Todo</button>
     </form>
   );
